340982ce6fb7aadb22ee21c8a3a56803ddecd3d1
Sangyoon Lee <lsangyoon7454@gmail.com>
2024-12-12 22:08:45 -0600

    Initial commit: Upload local website files

--- __deploy.sh
+++ __deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Upload to server
-scp -r _site/* msaveski@ovid.u.washington.edu:public_html/
+scp -r _site/* sangyoonlee@umn.edu:public_html/
 
 # make sure to fix the permissions on the server
 # chmod -R 750 *
